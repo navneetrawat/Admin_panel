1a120e30b4acba6aa8fe4c8eb3bb7aa2ca24ff85
kharola97 <akharola4@gmail.com>
2025-08-08 20:45:06 +0530

    ankit-tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-// import Navbar from "./Components/Navbar"
-// import React from "react";
-// import StatsCard from "./Components/StatsCard";
-// function App() {
-//   return (
-//     <div className="bg-neutral-300 h-screen w-full">
-//       <Navbar/>
-//       <StatsCard/>
-//     </div>
-//   )
-// }
 
-// export default App;
 
 import React, { useState } from "react";
-// import Navbar from "./Components./Navbar";
-// import Sidebar from "./Components./Sidebar";
-// import StatsCards from "./Components./StatsCards";
+
 import Sidebar from "./Components/Sidebar";
 import Navbar from "./Components/Navbar";
 import StatsCards from "./Components/StatsCard"
@@ -25,10 +11,10 @@ export default function App() {
 
   return (
     <div className="flex">
-      {/* Sidebar */}
+      
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={() => setSidebarOpen(!isSidebarOpen)} />
 
-      {/* Main content */}
+     
       <div className={`flex-1 transition-all duration-300 ${isSidebarOpen ? "ml-60" : "ml-16"}`}>
         <Navbar toggleSidebar={() => setSidebarOpen(!isSidebarOpen)} />
         <StatsCards />
